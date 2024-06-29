0628fa421ba859219d83371db47b9d3c52c86a6c
swethamagesh22 <105511436+swethamagesh22@users.noreply.github.com>
2024-06-29 18:37:54 +0530

    AutocorrectwordAI.py

--- AutocorrectwordAI.py
+++ AutocorrectwordAI.py
@@ -4,7 +4,7 @@ spell = Speller(lang='en')
 def autocorrect_text(input_text):
     corrected_text = spell(input_text)
     return corrected_text
-# Create an interactive loop
+
 while True:
     input_text = input("Enter your text: ")
     if input_text.lower() == "exit":
